a1af8dd53dcfb12779ebcd21bbfa1d4ac5d99252
Pedro Luis Tapia <99137141+PedroTapia98@users.noreply.github.com>
2026-04-07 14:21:19 -0600

    scale

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ const modelTransform = {
     rotateX: modelRotate[0],
     rotateY: modelRotate[1],
     rotateZ: modelRotate[2],
-    scale: modelAsMercatorCoordinate.meterInMercatorCoordinateUnits()
+    scale: modelAsMercatorCoordinate.meterInMercatorCoordinateUnits() * 0.1
 };
 
 map.on('style.load', () => {
